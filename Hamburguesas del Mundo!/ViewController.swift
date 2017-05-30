47f55f0aee6712151dcd4788debe3d0de725ffa5
SergioGM12 <sergiogarciamartin@macbook-air-de-sergio.local>
2017-05-30 20:55:38 +0200

    Initial Commit

---
+++ Hamburguesas del Mundo!/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hamburguesas del Mundo!
+//
+//  Created by Sergio García Martín on 30/5/17.
+//  Copyright © 2017 Sergio García Martín. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
